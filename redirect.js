a9c14819c7aa2bac37caeeabd3167251ea0ed86d
nizamike229 <nizamike229@mail.ru>
2025-03-10 10:40:01 +0500

    made new adapt

--- redirect.js
+++ redirect.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const whatsappUrl = "https://api.whatsapp.com/send?phone=77776814389&text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5!%20%D0%9C%D0%B5%D0%BD%D1%8F%20%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B5%D1%81%D1%83%D0%B5%D1%82%20%D0%B2%D0%B0%D1%88%D0%B0%20%D0%BF%D1%80%D0%BE%D0%B4%D1%83%D0%BA%D1%86%D0%B8%D1%8F";
+    
     document.body.addEventListener('click', function(e) {
         const link = e.target.closest('a');
         if (!link) return;
@@ -6,15 +8,10 @@ document.addEventListener('DOMContentLoaded', function() {
         const href = link.getAttribute('href');
         if (!href) return;
         
-        // Проверяем, содержит ли ссылка домен minternational
-        if (href.includes('minternational')) {
+        // Проверяем ссылки на minternational или api.whatsapp
+        if (href.includes('minternational') || href.includes('api.whatsapp')) {
             e.preventDefault();
-            
-            // Плавная прокрутка к секции контактов
-            const contactSection = document.querySelector('.node96');
-            if (contactSection) {
-                contactSection.scrollIntoView({ behavior: 'smooth' });
-            }
+            window.location.href = whatsappUrl;
         }
     });
 }); 
